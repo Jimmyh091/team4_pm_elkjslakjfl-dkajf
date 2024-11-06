7fc13429745259f328043ac6c8577655297dd0b0
Jaime <164157880+Jimmyh091@users.noreply.github.com>
2024-11-06 16:23:57 +0100

    lñasdfjklfa

---
+++ app/src/main/java/com/example/team4_pm_ejejjjejjejjsjssjsjsss/ej6/main.kt
@@ -0,0 +1,16 @@
+package com.example.team4_pm_ejejjjejjejjsjssjsjsss.ej6
+
+import com.example.team4_pm_ejejjjejjejjsjssjsjsss.ej1.Planeta
+
+fun main(){
+    val sistemaSolar = listOf(
+        Planeta("Mercurio", "Rocoso", 3.2f),
+        Planeta("Venus", "Rocoso", 4.8f),
+        Planeta("Tierra", "Rocoso", 5.9f),
+        Planeta("Marte", "Rocoso", 6.3f),
+        Planeta("Jupiter", "Gaseoso", 189.8f),
+        Planeta("Saturno", "Gaseoso", 50.6f),
+        Planeta("Urano", "Gaseoso", 8.6f),
+        Planeta("Neptuno", "Gaseoso", 1.4f)
+    )
+}
