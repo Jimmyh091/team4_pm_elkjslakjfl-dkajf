71a417da3b28408052df1d23d10fe1f07f8ff596
Jimmyh091 <jaimecaballero164@gmail.com>
2024-11-26 19:41:49 +0100

    sa

--- app/src/main/java/com/example/team4_pm_ejejjjejjejjsjssjsjsss/ej14/Circunferencia.kt
+++ app/src/main/java/com/example/team4_pm_ejejjjejjejjsjssjsjsss/ej14/Circunferencia.kt
@@ -1,7 +1,16 @@
 package com.example.team4_pm_ejejjjejjejjsjssjsjsss.ej14
 
-class Circunferencia (radio : Float ) {
+class Circunferencia (var radio : Float ) {
+
+    val PI = 3.141592
 
     constructor(centro : Pair<Float, Float>, puntoCircunferencia : Pair<Float, Float>) : this((puntoCircunferencia.first - centro.first) * (puntoCircunferencia.first - centro.first) + (puntoCircunferencia.second - centro.second) * (puntoCircunferencia.second - centro.second))
 
+    fun longitud() : Float {
+        return (radio * 2 * PI).toFloat()
+    }
+
+    override fun toString(): String {
+        return "La circunferencia tiene como radio $radio y como longitud ${longitud()}"
+    }
 }
---
+++ app/src/main/java/com/example/team4_pm_ejejjjejjejjsjssjsjsss/ej14/main.kt
@@ -0,0 +1,6 @@
+package com.example.team4_pm_ejejjjejjejjsjssjsjsss.ej14
+
+fun main(){
+    var cirucfenneenerenica = Circunferencia(Pair<Float, Float>(0f,0f), Pair<Float, Float>(30f,30f))
+    print(cirucfenneenerenica.toString())
+}
